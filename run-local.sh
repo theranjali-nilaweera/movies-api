e105850bdf449807d4d409a607d886a1830873c0
theranjali-nilaweera <theranjali@gmail.com>
2019-06-11 11:02:47 +1000

    Update run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -3,5 +3,5 @@ echo "Change nvm"
 source ~/.profile
 nvm use || { echo "Error changing node version"; exit 1; }
 
-echo "Starting up local weather01 API"
-npm run start-serve || { echo "Error running local weather01 API"; exit 1; }
+echo "Starting up local movie API"
+npm run start-serve || { echo "Error running local movie API"; exit 1; }
